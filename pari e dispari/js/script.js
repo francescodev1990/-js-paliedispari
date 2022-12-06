52e6f316c744149cec985e351261719724f3ebfe
Your Name <almannofrancesco@gmail.com>
2022-12-06 10:07:51 +0100

    function insert

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -1,1 +1,6 @@
 let numero = prompt('Per favore inserisci un numero che sia compreso tra 1 e 5');
+
+function casuale() {
+    num = Math.round(Math.random() * 5) ;
+    document.getElementById("casuale").innerHTML ="Numero casuale " + num;
+}
