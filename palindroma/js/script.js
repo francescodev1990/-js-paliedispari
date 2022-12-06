f334760cf4f5c2148f64ca25aa7cda0ade72a5c1
Your Name <almannofrancesco@gmail.com>
2022-12-06 09:46:06 +0100

    js modified

---
+++ palindroma/js/script.js
@@ -0,0 +1,17 @@
+prompt('Per favore inserisci una parola');
+
+let parola = 'palindroma';
+
+let parolaInversa = invertiParola;
+
+if(parola == parolaInversa){
+    console.log('la parola è palindroma');
+}
+else{
+    console.log('la parola non è palindroma');
+}
+
+function invertiParola(str){
+    let strInversa = str.split('').reverse().join('');
+    return strInversa;
+}
